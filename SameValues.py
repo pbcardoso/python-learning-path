d457ff4b10e35f355b4763cfab9a728f3e93fc03
l2j_git <paulomsdos@gmail.com>
2025-01-29 19:17:41 +0000

    Refactor variable names in SameValues.py for consistency and clarity

--- SameValues.py
+++ SameValues.py
@@ -1,13 +1,13 @@
 # A program that reads two values and prints the largest one, not allowing the values to be equal.
 
-first_value = int(input('First value: '))
-second_value = int(input('\nSecond value: '))
+firstValue = int(input('First value: '))
+secondValue = int(input('\nSecond value: '))
 
-if first_value == second_value:
+if firstValue == secondValue:
   print('\nThe two values entered are the same!')
 
-elif second_value > first_value:
-  print('\nThe higher value is: \033[31m{}\033[m'.format(second_value))
+elif secondValue > firstValue:
+  print('\nThe higher value is: \033[31m{}\033[m'.format(secondValue))
 
 else:
-  print('\nThe higher value is: \033[31m{}\033[m'.format(first_value))
+  print('\nThe higher value is: \033[31m{}\033[m'.format(firstValue))
