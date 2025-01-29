572cc4d91193aab3cb6461ebe7f909e8b24e1eb2
l2j_git <paulomsdos@gmail.com>
2025-01-29 19:06:55 +0000

    Add SameValues.py to compare two input values and display results

---
+++ SameValues.py
@@ -0,0 +1,11 @@
+first_value = int(input('First value: '))
+second_value = int(input('\nSecond value: '))
+
+if first_value == second_value:
+  print('\nThe two values entered are the same!')
+
+elif second_value > first_value:
+  print('\nThe higher value is: \033[31m{}\033[m'.format(second_value))
+
+else:
+  print('\nThe higher value is: \033[31m{}\033[m'.format(first_value))
