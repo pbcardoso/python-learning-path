75a1a60df847a6b26b0816665840c4d6f961260d
l2j_git <paulomsdos@gmail.com>
2025-01-30 09:58:07 +0000

    Refactor output formatting in sort_three_numbers.py for improved readability

--- sort_three_numbers.py
+++ sort_three_numbers.py
@@ -4,20 +4,22 @@ val_one = int(input("Enter the first integer: "))
 val_two = int(input("Enter the second integer: "))
 val_three = int(input("Enter the third integer: "))
 
+format_string = '\n{}, {}, {}.'
+
 if val_one >= val_two and val_two >= val_three:
-    print('\n{}, {}, {}.'.format(val_three, val_two, val_one))
+    print(format_string.format(val_three, val_two, val_one))
 
 elif val_two >= val_one and val_one >= val_three:
-    print('\n{}, {}, {}.'.format(val_three, val_one, val_two))
+    print(format_string.format(val_three, val_one, val_two))
 
 elif val_three >= val_one and val_one >= val_two:
-    print('\n{}, {}, {}.'.format(val_two, val_one, val_three))
+    print(format_string.format(val_two, val_one, val_three))
 
 elif val_one >= val_three and val_three >= val_two:
-    print('\n{}, {}, {}.'.format(val_two, val_three, val_one))
+    print(format_string.format(val_two, val_three, val_one))
 
 elif val_two >= val_three and val_three >= val_one:
-    print('\n{}, {}, {}.'.format(val_one, val_three, val_two))
+    print(format_string.format(val_one, val_three, val_two))
 
 else:
-    print('\n{}, {}, {}.'.format(val_one, val_two, val_three))
+    print(format_string.format(val_one, val_two, val_three))
